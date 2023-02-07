dbcc1cf649c3460f9613ae2266bc06711185ba11
caio <caio.lima@youxgroup.com.br>
2023-02-07 01:22:22 -0300

    Ajeitando index

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,12 +1,44 @@
-
 <template>
-  <h1>Aqui e a Home!</h1>
+  <custom-header 
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker @ 2023</p>
+  </div>
 </template>
 
 <script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+
 export default {
-  
+  components: { customHeader, Contact },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedback' })
+      }
+    })
+    
+    function handleLogin () {
+    
+    }
+    function handleAccountCreate () {
+
+    }
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
 }
 </script>
 <style scoped>
-</style>
+</style>''
